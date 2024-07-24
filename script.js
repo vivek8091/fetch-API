dcf419663d2018f4a3c5d047d6dd2b738f0d651a
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2024-07-24 18:39:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+
+// Using Async & Await... (Much simplier and easy to read)
+// const getFacts = async () => {
+//     let response = await fetch(URL);
+//     // console.log(response);
+//     let data = await response.json();
+//     console.log(data[0]);
+//     factPara.innerText = data[1].text;
+// }
+
+
+
+//  Using Promice Chaining...
+const getFacts = () => {
+    fetch(URL).then((res) => {
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        factPara.innerText = data[2].text;
+    });
+}
+btn.addEventListener("click",getFacts);
